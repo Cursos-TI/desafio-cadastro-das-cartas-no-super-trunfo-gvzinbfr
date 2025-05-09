c8cccda0d9909bd1bebee86332ef22182031a856
gvzinbfr <alaskedhh@gmail.com>
2025-05-08 23:43:24 -0300

    Desenvolvi o SuperTrunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,95 +1,90 @@
 #include <stdio.h>
 
-int main(){
+int main() {
+    // Carta 1
+    char estado;
+    char codigo[7];
+    char nomecidade[77];
+    int populacao;
+    float area;
+    float pib;
+    int pontosturisticos;
 
-// carta 1
-char estado;
-char codigo[7];
-char nomecidade[77];
-int populacao;
-float area;
-float pib;
-int pontosturisticos;
+    // Carta 2
+    char estado2;
+    char codigo2[7];
+    char nomecidade2[77];
+    int populacao2;
+    float area2;
+    float pib2;
+    int pontosturisticos2;
 
-// carta 2
-char estado2;
-char codigo2[7];
-char nomecidade2[77];
-int populacao2;
-float area2;
-float pib2;
-int pontosturisticos2;
+    // Entrada da Carta 1
+    printf("Digite os dados da Carta 1:\n");
 
-//carta 1
-printf("Digite os dados da Carta 1:\n");
+    printf("Estado (letra de A a H): ");
+    scanf(" %c", &estado);
 
-printf("Estado (letra de A a H): ");
-scanf(" %c", &estado);
+    printf("Código da Carta (ex: A01): ");
+    scanf("%s", codigo);
 
-printf("Código da Carta (ex: A01): ");
-scanf("%s", codigo);
+    printf("Nome da Cidade: ");
+    scanf(" %[^\n]", nomecidade);
 
-printf("Nome da Cidade: ");
-scanf(" [^\n]", nomecidade);
+    printf("População: ");
+    scanf("%d", &populacao);
 
-printf("População: ");
-scanf("%d", &populacao);
+    printf("Área (em km²): ");
+    scanf("%f", &area);
 
-printf("Área (em km²): ");
-scanf("%f", &area);
+    printf("PIB (em bilhões de reais): ");
+    scanf("%f", &pib);
 
-printf("PIB (em bilhões de reais): ");
-scanf("%f", &pib1);
+    printf("Número de Pontos Turísticos: ");
+    scanf("%d", &pontosturisticos);
 
-printf("Número de Pontos Turísticos: ");
-scanf("%d", &pontosturisticos);
+    // Entrada da Carta 2
+    printf("\nDigite os dados da Carta 2:\n");
 
-//carta 2
-printf("\nDigite os dados da Carta 2:\n");
+    printf("Estado (letra de A a H): ");
+    scanf(" %c", &estado2);
 
-printf("Estado (letra de A a H): ");
-scanf(" %c", estado2);
+    printf("Código da Carta (ex: B03): ");
+    scanf("%s", codigo2);
 
-printf("Código da Carta (ex: B03): ");
-scanf("%s", codigo2);
+    printf("Nome da Cidade: ");
+    scanf(" %[^\n]", nomecidade2);
 
-printf("Nome da Cidade: ");
-scanf(" [^\n]", nomecidade2);
+    printf("População: ");
+    scanf("%d", &populacao2);
 
-printf("População: ");
-scanf("%d", &populacao2);
+    printf("Área (em km²): ");
+    scanf("%f", &area2);
 
-printf("Área (em km²): ");
-scanf("%f", &area2);
+    printf("PIB (em bilhões de reais): ");
+    scanf("%f", &pib2);
 
-printf("PIB (em bilhões de reais): ");
-scanf("%f", &pib2);
+    printf("Número de Pontos Turísticos: ");
+    scanf("%d", &pontosturisticos2);
 
-printf("Número de Pontos Turísticos: ");
-scanf("%d", &pontosTuristicos2);
+    // Exibição das Cartas
+    printf("\nCarta 1:\n");
+    printf("Estado: %c\n", estado);
+    printf("Código: %s\n", codigo);
+    printf("Nome da Cidade: %s\n", nomecidade);
+    printf("População: %d\n", populacao);
+    printf("Área: %.2f km²\n", area);
+    printf("PIB: %.2f bilhões de reais\n", pib);
+    printf("Número de Pontos Turísticos: %d\n", pontosturisticos);
 
-//cartas
-printf("\nCarta 1:\n");
-printf("Estado: %c\n", estado1);
-printf("Código: %s\n", codigo1);
-printf("Nome da Cidade: %s\n", nomecidade);
-printf("População: %d\n", populacao1);
-printf("Área: %.2f km²\n", area1);
-printf("PIB: %.2f bilhões de reais\n", pib1);
-printf("Número de Pontos Turísticos: %d\n", pontosturisticos);
+    printf("\nCarta 2:\n");
+    printf("Estado: %c\n", estado2);
+    printf("Código: %s\n", codigo2);
+    printf("Nome da Cidade: %s\n", nomecidade2);
+    printf("População: %d\n", populacao2);
+    printf("Área: %.2f km²\n", area2);
+    printf("PIB: %.2f bilhões de reais\n", pib2);
+    printf("Número de Pontos Turísticos: %d\n", pontosturisticos2);
 
-printf("\nCarta 2:\n");
-printf("Estado: %c\n", estado2);
-printf("Código: %s\n", codigo2);
-printf("Nome da Cidade: %s\n", nomecidade2);
-printf("População: %d\n", populacao2);
-printf("Área: %.2f km²\n", area2);
-printf("PIB: %.2f bilhões de reais\n", pib2);
-printf("Número de Pontos Turísticos: %d\n", pontosturisticos2);
-
-
-//GUSTAVO JESUS DOS SANTOS//
-
-return 0;
-
+    return 0;
 }
